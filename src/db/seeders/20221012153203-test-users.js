ad835688fdc51e6241e4240c83f26d027798743d
veselov1209 <veselov1209@gmail.com>
2022-10-13 13:10:13 +0300

    seeders users

--- src/db/seeders/20221012153203-test-users.js
+++ src/db/seeders/20221012153203-test-users.js
@@ -1,25 +1,30 @@
 'use strict';
+const bcrypt = require('bcrypt')
+
+function generateTEstUsers(){
+  const users = [];
+  for(let i = 0; i<100; i++){
+    users.push({
+      firstName: `Test${i}`,
+      lastName: `Surname${i}`,
+      email: `test${i}@gmail.com`,
+      login: `login${i}`,
+      age: `${i}`,
+      passwordHash: bcrypt.hashSync(`admin${i}`, 10),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    });
+  }
+  return users;
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert("Users", generateTEstUsers(), {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("Users", null, {});
   }
 };
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// const {User} = require('./db/models');
+
+// // User.create({
+// //     name: "testname2",
+// //     surname: "testsurname2",
+// //     email: "test@gmail.com"
+// // }).then(console.log);
+
+// // User.findByPk(2).then(console.log);
+
+// // User.findAll().then(console.log);
+
+
+
+// User.findAll({attributes: {exclude: ["email", "surname"]}}).then(console.log);
+
