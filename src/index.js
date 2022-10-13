de76cc07571758250c1bd8d8c527bade6c4d2165
veselov1209 <veselov1209@gmail.com>
2022-10-13 19:33:01 +0300

    env babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const {User} = require('./db/models');
+const {User, Task} = require('./db/models');
 const bcrypt = require('bcrypt');
 // // User.create({
 // //     name: "testname2",
@@ -102,3 +102,124 @@ const updateUser = async (id, data) => {
 
 // getUserById(2).then(console.log).catch(console.err);
 // updateUser(2, {email: "newemail2@mail.com", age: 333}).then(console.log).catch(console.err);
+
+
+//-----------------------TASKS--------
+
+
+const createTask = async data => {
+    try{
+        const newTask = await Task.create(data);
+        return newTask;
+    } catch(e){
+        throw e;
+    }
+}
+
+const getTaskById = async id => {
+    try{
+        const foundTask = await Task.findByPk(id);
+        return foundTask;
+    } catch(e){
+        throw e;
+    }
+}
+
+const deleteTaskById = async taskId => {
+    try{
+        const foundTask = await Task.destroy({
+            where: {
+                id: taskId            }
+        });
+        return foundTask;
+    } catch(e){
+        throw e;
+    }
+}
+
+
+const updateTaskById = async (id, data) => {
+    try{
+        const [updatedRowCount, updatedRows] = await Task.update(data, {
+            where: {id},
+            returning: true
+        });
+        if(updatedRowCount){
+            const task = updatedRows[0].get();
+            return task;
+        }
+    } catch(e){
+        throw e;
+    }
+}
+
+
+
+const getAllTasks = async data => {
+    const {limit, offset} = data;
+    try{
+        const tasks = await Task.findAll({
+            limit,
+            offset,
+            order: ['deadline', 'ASC'],
+            attributes: {
+                exclude: ['createdAt', 'updatedAt']
+            }
+        });
+        return tasks;
+    } catch(e){
+        throw e;
+    }
+}
+
+
+const testTask = {
+    UserId: 10,
+    name: 'Test task for 10 user',
+    isDone: false,
+    deadline: '2022-10-10 14:00'
+};
+
+
+//createTask(testTask).then(console.log).catch(console.err);
+//getTaskById(1).then(console.log).catch(console.err);
+//updateTaskById(1, {isDone: true}).then(console.log).catch(console.err);
+//getTaskById(1).then(console.log).catch(console.err);
+
+const getTaskByUserId = async (userId) => {
+    try{
+        const tasks = await Task.findAll({
+            order: ['deadline'],
+            attributes: {
+                exclude: ['createdAt', 'updatedAt']
+            },
+            where: {
+                UserId: userId
+            }
+        });
+        return tasks;
+    } catch(e){
+        throw e;
+    }
+}
+
+// getTaskByUserId(10).then(console.log).catch(console.err);
+
+async function getUsersWithTasks() {
+    try{
+        const res = User.findAll({
+            attributes:{
+                exclude: ['passwordHash']
+            },
+            include: [{
+                model: Task
+            }]
+        });
+        //return res.map(i => i.get());
+        return res;
+    }catch(e){
+        throw e;
+    }
+}
+
+getUsersWithTasks().then(console.log);
